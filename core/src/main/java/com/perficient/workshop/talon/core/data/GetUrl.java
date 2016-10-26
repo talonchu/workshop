814e6e1caf3829252aaa6b9a249f4ecd35ed84ed
talonchu <talon.chu@perficient.com>
2016-10-26 15:30:28 +0800

    init

---
+++ core/src/main/java/com/perficient/workshop/talon/core/data/GetUrl.java
@@ -0,0 +1,37 @@
+package com.perficient.workshop.talon.core.data;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.sling.commons.json.JSONException;
+import org.apache.sling.commons.json.JSONObject;
+
+import com.adobe.cq.sightly.WCMUse;
+
+public class GetUrl extends WCMUse	 {
+	Logger log = Logger.getLogger(GetUrl.class.getName());
+	@Override
+	public void activate() throws Exception {
+
+	}
+
+	public Map<String, String> getVanityPaths() {
+		Map<String, String> map = new HashMap<String, String>();
+
+		String[] strs = getProperties().get("definitions", new String[] {});
+
+		for (String str : strs) {
+			try {
+				JSONObject obj = new JSONObject(str);
+				map.put(obj.getString("term"), obj.getString("definition"));
+			} catch (JSONException e) {
+				log.log(Level.INFO, e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		return map;
+	}
+
+}
